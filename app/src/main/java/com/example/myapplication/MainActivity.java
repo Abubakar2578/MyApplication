09c1d1fae13ea747c9fdc3a1914c6fb7bc87d2fb
abubakar <mianabubaker007@gmail.com>
2023-05-17 01:37:02 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1 , btn2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.button);
+        btn2 = findViewById(R.id.button2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this , MainActivity2.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent git = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/Abubakar2578/"));
+                startActivity(git);
+
+
+
+            }
+        });
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -0,0 +1,78 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+
+    public void dialCall(View v){
+        String contact = "tel:+923004042213";
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(contact));
+        startActivity(intent);
+    }
+
+    public void sendDataToEmail(View v){
+        EditText title=(EditText) findViewById(R.id.myEditText);
+        EditText description=(EditText) findViewById(R.id.textArea);
+
+        String titleContent=title.getText().toString();
+        String descriptionContent=description.getText().toString();
+
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_SUBJECT,titleContent);
+        intent.putExtra(Intent.EXTRA_TEXT,descriptionContent);
+
+        intent.setType("message/rfc822");
+        intent.setPackage("com.google.android.gm");
+        try {
+            startActivity(intent);
+        } catch(ActivityNotFoundException e){
+            Toast.makeText(this, "Gmail is not installed on your device", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public void sendDataToWhatsapp(View v){
+
+
+        Intent what = new Intent(Intent.ACTION_VIEW);
+        EditText title=(EditText) findViewById(R.id.myEditText);
+        EditText description=(EditText) findViewById(R.id.textArea);
+
+        // getting the text and storing it in string
+        String titleContent=title.getText().toString();
+        String descriptionContent=description.getText().toString();
+        Intent intent=new Intent(Intent.ACTION_SEND);
+        String details=titleContent +"\n"+descriptionContent;
+
+        String message = details;
+        String number = "tel:+923004042213";
+        what.setData(Uri.parse("whatsapp://send?phone=" + number + "&text=" + message));
+        startActivity(what);
+    }
+
+    public void moveback(View v){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
+
